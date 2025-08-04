44cceb71ccfff45a16df4a78e0af73ca3db5310f
Taweesaksangkorane <6731503012@lamduan.mfu.ac.th>
2025-08-04 09:17:04 +0000

    Done

--- src/main/java/th/mfu/MockWebClient.java
+++ src/main/java/th/mfu/MockWebClient.java
@@ -6,18 +6,30 @@ import java.net.*;
 // call mockup server at port 8080
 public class MockWebClient {
     public static void main(String[] args) {
-
+        try{
         // TODO: Create a socket to connect to the web server on port 8080
+         Socket socket = new Socket("localhost", 8080);
 
         // :TODO Create input and output streams for the socket
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
 
         // TODO: send an HTTP GET request to the web server
         String request = "GET / HTTP/1.1\r\nHost: localhost\r\n\r\n";
-
+            out.print(request);
+            out.flush();
         // Read the response from the web server and print out to console
+            String line;
+            while ((line = in.readLine()) != null) {
+                System.out.println(line);
+            }
 
         // Close the socket
+        socket.close();
 
+        }catch(IOException e) {
+            e.printStackTrace();
     }
 
+}
 }
